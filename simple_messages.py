74087aacc637d6af55a8bc3a95722337ee0ec0b6
aadilmeraj54 <aadilmeraj54@gmail.com>
2026-06-11 15:15:15 +0530

    Create simple_messages.py

---
+++ simple_messages.py
@@ -0,0 +1,9 @@
+message_from_Aadil = "Hey Aaliyaaa, i love you ❤️"
+message_from_Aadil2 = "Maan jaiyeee naaaa"
+
+print (message_from_Aadil)
+print (message_from_Aadil2)
+
+'i told my friend, "Python is my favorite language"'
+"The language 'python' is named after  monty python, not the snake."
+"one os python'sstreng is its diverse and supportive community"
