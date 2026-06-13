deb05427e93479e9ab93b4ac34249eaf1bf53e14
aadilmeraj54 <aadilmeraj54@gmail.com>
2026-06-13 21:11:54 +0530

    Create full_name.py

---
+++ full_name.py
@@ -0,0 +1,13 @@
+# first_name = "aadil"
+# last_name = "meraj"
+# full_name = f"{first_name} {last_name}"
+# print(f"Hello, {full_name.title()}!")
+
+
+first_name = "aadil"
+last_name = "meraj"
+
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}!")       
+
+print("hello woirld from {full_name.title()}")
