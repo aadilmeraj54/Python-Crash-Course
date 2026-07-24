a32834abeff9a7d1f473ad0409002a10eba43e7e
aadilmeraj54 <aadilmeraj54@gmail.com>
2026-07-24 20:19:47 +0530

    Create fString.py

---
+++ fString.py
@@ -0,0 +1,57 @@
+# name = "aaliya naaz"
+# print(name.title())
+
+
+# name = "Aadil Meraj"
+# print(name.upper())
+# print(name.lower())
+
+# first_name = "aaliya"
+# last_name = "naaz"
+# full_name = f"{first_name} {last_name}"
+# print(full_name)
+
+# first_name = "aaliya"
+# last_name = "naaz"
+# full_name = f"{first_name} {last_name}"
+# print(f"hello,{full_name.title()}!")
+
+# first_name = "aaliya"
+# last_name = "naaz"
+# full_name = f"{first_name} {last_name}"
+# message = f"hello,{full_name.title()}!"
+# print(message)
+
+
+
+# first_name = "aaliya"
+# last_name ="naaz"
+# full_name = f"{first_name} {last_name}"
+# message = f"hello, {full_name.title()}!"
+# print (message)
+
+
+
+# print("python")
+# print("\t python and my name is \t aadil")
+
+
+# print("my name is \t Aadil  \n i am from bettiah")
+
+# print ("Language: \npython\nC\nJavascript")
+
+
+# print("LAnguage: \n\tPython\n\tC\n\tJavascript")
+
+
+# print("pthon         " "python")
+
+# first_name = "Aadil"
+# last_name = "meraj"
+# full_name = f"hello {first_name} {last_name}!!!!"
+# print (full_name)
+
+
+favourate_language = 'python    '"hdd"
+favourate_language = favourate_language.rstrip()
+print(favourate_language)
