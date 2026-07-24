00a04e08baf1685a9bf8c8e55cd6d52e7cb7a858
aadilmeraj54 <aadilmeraj54@gmail.com>
2026-07-24 20:19:49 +0530

    Update full_name.py

--- full_name.py
+++ full_name.py
@@ -10,4 +10,4 @@ last_name = "meraj"
 full_name = f"{first_name} {last_name}"
 print(f"Hello, {full_name.title()}!")       
 
-print("hello woirld from {full_name.title()}")
+print(f"hello world from {full_name.title()}")
